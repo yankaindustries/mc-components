eb1af646f86b88e8fc5bf165a55c4e6f39f1ddcc
stabarez <stf9331@gmail.com>
2018-06-21 16:26:49 -0300

    Added CheckTile and ImageTile component. Added file-loader to webpack… (#28)
    * Added CheckTile and ImageTile component. Added file-loader to webpack configuration. Edit
    
    * PR fixes and code clean up
    
    * deleted images folder (renamed to assets)
    
    * removed eslint-plugin-react

---
+++ src/components/CheckTile/index.stories.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react'
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import shondaRhimesThumbnail from '../../assets/shonda-rhimes-video.png'
+
+import CheckTile from '../CheckTile'
+import RadioInput from "../RadioInput";
+
+// CheckTile Handler
+class CheckTileHandler extends Component {
+  state = { checked: false }
+
+  render () {
+    const { checked } = this.state
+
+    return (
+      <div style={{ maxWidth: '500px' }}>
+        <CheckTile
+          {...this.props}
+          checked={checked}
+          onCheck={() => this.setState({ checked: !checked })}
+        >
+        </CheckTile>
+      </div>
+    )
+  }
+}
+
+const infoProps = {
+  inline: true,
+  header: false,
+  propTables: [CheckTile],
+  propTablesExclude: [CheckTileHandler],
+}
+
+storiesOf('CheckTile', module)
+  .add('CheckTile - 1000x609',
+    withInfo({ ...infoProps })(() =>
+      <div style={{ maxWidth: '500px' }}>
+        <CheckTile
+          imageUrl={shondaRhimesThumbnail}
+          checked
+          aspectRatio='1000x609'
+          animationStyle='hover-zoom'
+          onCheck={() => {}}
+        >
+          <p>Content</p>
+        </CheckTile>
+      </div>,
+    ),
+  )
+  .add('CheckTile - 1000x609 - no animation',
+    withInfo({ ...infoProps })(() =>
+      <div style={{ maxWidth: '500px' }}>
+        <CheckTile
+          imageUrl={shondaRhimesThumbnail}
+          checked={false}
+          aspectRatio='1000x609'
+          onCheck={() => {}}
+        >
+          <p>Content</p>
+        </CheckTile>
+      </div>,
+    ),
+  )
+  .add('with handler',
+    withInfo({ ...infoProps })(() =>
+      <CheckTileHandler
+        imageUrl={shondaRhimesThumbnail}
+        aspectRatio='1000x609'
+      >
+        <p>Content</p>
+      </CheckTileHandler>,
+    ),
+  )
---
+++ src/components/ImageTile/index.stories.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import shondaRhimesThumbnail from '../../assets/shonda-rhimes-video.png'
+
+import ImageTile from '../ImageTile'
+
+const infoProps = {
+  inline: true,
+  header: false,
+}
+
+storiesOf('ImageTile', module)
+  .add('ImageTile 4x3',
+    withInfo({ ...infoProps })(() =>
+        <div style={{ maxWidth: '500px' }}>
+          <ImageTile
+            imageUrl={shondaRhimesThumbnail}
+            aspectRatio='4x3'
+            backgroundGradient={false}
+          >
+            <p>Content</p>
+          </ImageTile>
+        </div>,
+    ),
+  )
+  .add('ImageTile - 16x9 - backgroundGradient',
+    withInfo({ ...infoProps })(() =>
+      <div style={{ maxWidth: '500px' }}>
+        <ImageTile
+          imageUrl={shondaRhimesThumbnail}
+          aspectRatio='16x9'
+          backgroundGradient
+        >
+          <p>Content</p>
+        </ImageTile>
+      </div>,
+    ),
+  )
