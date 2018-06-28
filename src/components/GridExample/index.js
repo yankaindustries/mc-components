0ce7477af4458b94a6e206e0a1142477820de842
Stephen Pontes <stephen@masterclass.com>
2018-06-28 14:19:56 -0700

    Updating to inline styles

--- src/components/GridExample/index.js
+++ src/components/GridExample/index.js
@@ -1,78 +1,74 @@
 import React from 'react'
-import { number, string } from 'prop-types'
-import cn from 'classnames'
+const styles = {
+  background: '#ccc',
+  width: '100%',
+  height: '64px',
+  border: '1px solid #929093',
+}
 
-const GridExample = () => {
-
-  return (
-    <div className="container grid-example">
-      <div class="row align-items-center">
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-md">
-          <div className="grid-example-block"></div>
-        </div>
+const GridExample = () => (
+  <div className='container grid-example'>
+    <div className='row align-items-center'>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
       </div>
-
-
-      <div class="row align-items-center">
-        <div class="col-sm-12 col-md-4">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-sm-12 col-md-4">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-sm-12 col-md-4">
-          <div className="grid-example-block"></div>
-        </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
       </div>
-
-      <div class="row align-items-center">
-        <div class="col-6">
-          <div className="grid-example-block"></div>
-        </div>
-        <div class="col-6">
-          <div className="grid-example-block"></div>
-        </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-md'>
+        <div className='grid-example-block' style={styles}></div>
       </div>
     </div>
 
-  )
-}
 
-GridExample.propTypes = {
-  className: string,
-}
+    <div className='row align-items-center'>
+      <div className='col-sm-12 col-md-4'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-sm-12 col-md-4'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-sm-12 col-md-4'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+    </div>
+
+    <div className='row align-items-center'>
+      <div className='col-6'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+      <div className='col-6'>
+        <div className='grid-example-block' style={styles}></div>
+      </div>
+    </div>
+  </div>
+)
 
 export default GridExample
