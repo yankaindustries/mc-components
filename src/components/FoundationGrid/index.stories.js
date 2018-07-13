000d4d81788618a834248500748715f8022b2886
Stephen Pontes <stephen@masterclass.com>
2018-07-13 13:42:46 -0700

    Implement Justins suggestions

--- src/components/FoundationGrid/index.stories.js
+++ src/components/FoundationGrid/index.stories.js
@@ -2,12 +2,69 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 
-import FoundationGrid from '../FoundationGrid'
+import './_styles.css'
 
 storiesOf('foundation|Grid', module)
-  .add(
-    'default',
-    withInfo()(() =>
-      <FoundationGrid />,
-    ),
-  )
+  .add('columns', withInfo()(() => (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-xs-6'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-xs-6'>
+            <div className='example-grid-block'></div>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-sm-12 col-md-4'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-sm-12 col-md-4'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-sm-12 col-md-4'>
+            <div className='example-grid-block'></div>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+          <div className='col-md'>
+            <div className='example-grid-block'></div>
+          </div>
+        </div>
+      </div>
+  )))
