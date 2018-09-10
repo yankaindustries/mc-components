dc2b8ccb41cf1c53cf5c8682c52971ce8357c4f9
Stephen Pontes <stephen@masterclass.com>
2018-09-10 11:34:38 -0700

    Starting on first round of checkbox styles

---
+++ src/components/Checkbox/index.stories.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+storiesOf('components|Forms/Checkbox', module)
+  .add('default', () => (
+    <div className='example-mc-type'>
+      <div className='container example--section'>
+        <h2 className='mc-text-d1'>Checkboxes</h2>
+        <p>And their various states</p>
+      </div>
+      <div className='container'>
+        <div className='align-items-center example--section'>
+          <h5 className='mc-text-h5'>Default State</h5>
+          {/*
+            - Entire inpiut wrapped with mc-input-checkbox
+            - aria-labelledby is set to the ID of the label
+            - htmlFor on the label is set to the id of the input
+          */}
+          <p className='mc-input-checkbox'>
+            <span
+              className='mc-input-checkbox__fauxbox'
+              role='checkbox'
+              aria-checked='false'
+              tabIndex='0'
+              aria-labelledby='agrees-to-terms-label'
+            >
+            </span>
+            <input type='checkbox' id='agrees-to-terms' className='mc-input-checkbox__realbox' />
+            <label className='mc-input-checkbox__label' id='agrees-to-terms-label' htmlFor='agrees-to-terms'>I agree to the terms</label>
+          </p>
+          <p className='mc-text--muted mc-text--monospace'>.mc-input-checkbox</p>
+        </div>
+
+        <div className='align-items-center example--section'>
+          <h5 className='mc-text-h5'>Checked State</h5>
+          <p className='mc-input-checkbox mc-input-checkbox--checked'>
+            {/*
+              - aria-checked is set to 'true'
+              - parent class of mc-input-checkbox--checked is added
+            */}
+            <span
+              className='mc-input-checkbox__fauxbox'
+              role='checkbox'
+              aria-checked='true'
+              tabIndex='0'
+              aria-labelledby='checks-things-twice-label'
+            >
+            </span>
+            <input type='checkbox' id='checks-things-twice' className='mc-input-checkbox__realbox' checked='checked' />
+            <label className='mc-input-checkbox__label' id='checks-things-twice-label' htmlFor='checks-things-twice'>I like checking things twice</label>
+          </p>
+          <p className='mc-text--muted mc-text--monospace'>.mc-input-checkbox--checked</p>
+        </div>
+      </div>
+    </div>
+  ))
