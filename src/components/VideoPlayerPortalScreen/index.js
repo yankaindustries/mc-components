5c331d218e305b4e3905eac160febb05028ca692
Bernabe Gonzalez <bernabe@rootstrap.com>
2018-09-18 10:49:55 -0300

    fix component name

--- src/components/VideoPlayerPortalScreen/index.js
+++ src/components/VideoPlayerPortalScreen/index.js
@@ -2,7 +2,7 @@ import { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { node, oneOfType, arrayOf, instanceOf } from 'prop-types'
 
-export default class ModalPortal extends Component {
+export default class VideoPlayerPortalScreen extends Component {
   static propTypes = {
     children: oneOfType([arrayOf(node), node]).isRequired,
     videoRoot: instanceOf(Element),
