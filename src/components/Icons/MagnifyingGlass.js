c52f56f8e1005463f9eaeeef5bccf704dbebd8de
Stephen Pontes <stephen@masterclass.com>
2018-09-20 15:27:02 -0700

    WIP

---
+++ src/components/Icons/MagnifyingGlass.js
@@ -0,0 +1,21 @@
+/* eslint jsx-quotes: 0 */
+import React from 'react'
+
+const MagnifyingGlass = props => (
+  <svg viewBox="0 0 14 14" fill="none" width="1em" height="1em" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M6 1.75a4.25 4.25 0 1 0 0 8.5 4.25 4.25 0 0 0 0-8.5zM.25 6a5.75 5.75 0 1 1 11.5 0A5.75 5.75 0 0 1 .25 6z"
+      fill="#DBDDE1"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.47 9.47a.75.75 0 0 1 1.06 0l3 3a.75.75 0 1 1-1.06 1.06l-3-3a.75.75 0 0 1 0-1.06z"
+      fill="#DBDDE1"
+    />
+  </svg>
+)
+
+export default MagnifyingGlass
