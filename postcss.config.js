0a3ababc73cbc541f2c717cd75ac99f88e32aca9
Stephen Pontes <stephen@masterclass.com>
2018-09-25 14:55:11 -0700

    Updating dist folder

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,16 @@ const cssnano = require('cssnano')
 const autoprefixer = require('autoprefixer')
 const inlineSVG = require('postcss-inline-svg')
 
-module.exports = {
-  plugins: [
-    autoprefixer({ grid: true }),
-    cssnano({ preset: 'default' }),
-    inlineSVG({ path: './src/assets' })
-  ],
-}
+module.exports = ctx => ({
+  syntax: ctx.env === 'development' ? 'postcss-scss' : undefined,
+  plugins: ctx.env === 'development'
+    ? [
+      autoprefixer({ grid: true }),
+      inlineSVG({ path: './src/assets' }),
+    ]
+    : [
+      autoprefixer({ grid: true }),
+      cssnano({ preset: 'default' }),
+      inlineSVG({ path: './src/assets' }),
+    ],
+})
