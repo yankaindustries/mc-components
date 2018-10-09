60bbf6978dfcc8d1a40367328e2ddc7439f67662
Justin Jones <justinjones53@gmail.com>
2018-10-09 13:37:19 -0700

    Adding color to <Background />

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import cn from 'classnames'
 
 import { renderChildren as renderProp } from '../helpers'
 
@@ -11,25 +12,31 @@ export default class Background extends PureComponent {
       PropTypes.arrayOf(PropTypes.node),
     ]),
     className: PropTypes.string,
+    color: PropTypes.oneOf([
+      'dark',
+      'medium',
+      'light',
+      'transparent',
+    ]),
     element: PropTypes.oneOfType([
       PropTypes.func,
       PropTypes.node,
-    ]).isRequired,
+    ]),
     fit: PropTypes.oneOf([
+      'background',
       'container',
       'content',
-      'background',
     ]),
     position: PropTypes.oneOf([
-      'left top',
-      'center top',
-      'right top',
-      'left center',
-      'center center',
-      'right center',
-      'left bottom',
       'center bottom',
+      'center center',
+      'center top',
+      'left bottom',
+      'left center',
+      'left top',
       'right bottom',
+      'right center',
+      'right top',
     ]),
     size: PropTypes.oneOf([
       'contain',
@@ -44,15 +51,27 @@ export default class Background extends PureComponent {
     size: 'none',
   }
 
-  state = {
-    loaded: false,
-  }
-
   constructor (props) {
     super(props)
 
     this.container = React.createRef()
     this.background = React.createRef()
+  }
+
+  state = {
+    loaded: false,
+  }
+
+  componentWillUnmount () {
+    if (this.props.element) {
+      window.removeEventListener('resize', this.setSizeBy)
+    }
+  }
+
+  onLoad = () => {
+    this.setSizeBy()
+    this.setState({ loaded: true })
+    window.addEventListener('resize', this.setSizeBy.bind(this))
   }
 
   setSizeBy = () => {
@@ -68,20 +87,11 @@ export default class Background extends PureComponent {
     }
   }
 
-  onLoad = () => {
-    this.setSizeBy()
-    this.setState({ loaded: true })
-    window.addEventListener('resize', this.setSizeBy.bind(this))
-  }
-
-  componentWillUnmount () {
-    window.removeEventListener('resize', this.setSizeBy)
-  }
-
   render () {
     const {
       children,
       className,
+      color,
       element,
       fit,
       position,
@@ -95,16 +105,17 @@ export default class Background extends PureComponent {
 
     const [x, y] = position.split(' ')
 
-    const classes = [
-      'mc-background',
-      loaded ? 'mc-background--loaded' : '',
-      `mc-background--position-x-${x}`,
-      `mc-background--position-y-${y}`,
-      `mc-background--fit-${fit}`,
-      `mc-background--size-${size}`,
-      `mc-background--size-${size}-${sizeBy}`,
-      className,
-    ].join(' ')
+    const classes = cn({
+      [className]: className,
+      'mc-background': true,
+      [`mc-background--color-${color}`]: color,
+      'mc-background--loaded': loaded,
+      [`mc-background--position-x-${x}`]: element,
+      [`mc-background--position-y-${y}`]: element,
+      [`mc-background--fit-${fit}`]: element,
+      [`mc-background--size-${size}`]: element,
+      [`mc-background--size-${size}-${sizeBy}`]: element,
+    })
 
     return (
       <div
@@ -112,7 +123,7 @@ export default class Background extends PureComponent {
         ref={this.container}
       >
         <div className='mc-background__background-container'>
-          {renderProp(element, {
+          {element && renderProp(element, {
             className: 'mc-background__background',
             ref: this.background,
             onLoad: this.onLoad,
--- src/components/Background/index.stories.js
+++ src/components/Background/index.stories.js
@@ -15,78 +15,115 @@ storiesOf('components|Background', module)
     <div className='container'>
       <h2 className='mc-text-h2'>
         Background
+        {' '}
         <Badge count='Experimental' />
       </h2>
 
-      <DocSection title='Examples'>
-        <CodeExample>
-          <Background
-            element={<img src={shondaRhimesThumbnail} />}
-            fit='content'
-            position='center top'
-            size='cover'
-          >
-            <br /><br /><br />
-            <div className='row'>
-              <div className='col-4 offset-1'>
-                <h2 className='mc-text-h2'>
-                  Some Title
-                </h2>
+      <div className='row'>
+        <div className='col-lg-6'>
+          <DocSection title='Colors'>
+            <CodeExample>
+              <Background color='light'>
+                <br />
+                <br />
+                <br />
+              </Background>
+            </CodeExample>
 
-                <p>
-                  A lot of information about cool things that you should like.
-                </p>
-              </div>
-            </div>
-            <br /><br /><br />
-          </Background>
-        </CodeExample>
+            <CodeExample>
+              <Background color='medium'>
+                <br />
+                <br />
+                <br />
+              </Background>
+            </CodeExample>
 
-        <CodeExample>
-          <Background
-            element={<img src={shondaRhimesThumbnail} />}
-            fit='content'
-            position='right center'
-            size='contain'
-          >
-            <br /><br /><br />
-            <div className='row'>
-              <div className='col-4 offset-1'>
-                <h2 className='mc-text-h2'>
-                  Some Title
-                </h2>
+            <CodeExample>
+              <Background color='dark'>
+                <br />
+                <br />
+                <br />
+              </Background>
+            </CodeExample>
+          </DocSection>
+        </div>
 
-                <p>
-                  A lot of information about cool things that you should like.
-                </p>
-              </div>
-            </div>
-            <br /><br /><br />
-          </Background>
-        </CodeExample>
+        <div className='col-lg-6'>
+          <DocSection title='Images'>
+            <CodeExample>
+              <Background
+                element={<img src={shondaRhimesThumbnail} />}
+                fit='content'
+                position='center top'
+                size='cover'
+              >
+                <br />
+                <div className='row'>
+                  <div className='col-4 offset-1'>
+                    <h2 className='mc-text-h2'>
+                      Fit Content: Cover
+                    </h2>
 
-        <CodeExample>
-          <Background
-            element={<img src={shondaRhimesThumbnail} />}
-            fit='background'
-            position='center bottom'
-            size='contain'
-          >
-            <br /><br /><br />
-            <div className='row'>
-              <div className='col-4 offset-1'>
-                <h2 className='mc-text-h2'>
-                  Some Title
-                </h2>
+                    <p>
+                      The background element is scaled to fill the entire
+                      area behind the content, from the specified position.
+                    </p>
+                  </div>
+                </div>
+                <br />
+              </Background>
+            </CodeExample>
 
-                <p>
-                  A lot of information about cool things that you should like.
-                </p>
-              </div>
-            </div>
-            <br /><br /><br />
-          </Background>
-        </CodeExample>
-      </DocSection>
+            <CodeExample>
+              <Background
+                element={<img src={shondaRhimesThumbnail} />}
+                fit='content'
+                position='right center'
+                size='contain'
+              >
+                <br />
+                <div className='row'>
+                  <div className='col-4 offset-1'>
+                    <h2 className='mc-text-h2'>
+                      Fit Content: Contain
+                    </h2>
+
+                    <p>
+                      The background element is scaled to fit within the
+                      area behind the content, pinned to a specified position.
+                    </p>
+                  </div>
+                </div>
+                <br />
+              </Background>
+            </CodeExample>
+
+            <CodeExample>
+              <Background
+                element={<img src={shondaRhimesThumbnail} />}
+                fit='background'
+                position='center bottom'
+                size='contain'
+              >
+                <br />
+                <div className='row'>
+                  <div className='col-4 offset-1'>
+                    <h2 className='mc-text-h2'>
+                      Fit Background: Contain
+                    </h2>
+
+                    <p>
+                      The background element decides the size of the container,
+                      with the content sitting above it and pinned to a
+                      specific position.
+                    </p>
+                  </div>
+                </div>
+                <br />
+              </Background>
+            </CodeExample>
+          </DocSection>
+        </div>
+      </div>
     </div>
   )))
