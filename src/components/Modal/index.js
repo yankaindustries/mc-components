4f3a65bdbbefcfa2ea7bbd04b2e3a4620006cfc5
Justin Jones <justinjones53@gmail.com>
2018-10-15 15:44:08 -0700

    Building new <Modal /> component (#241)
    * Building new <Modal /> component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,91 @@
+import React, { PureComponent } from 'react'
+import { createPortal } from 'react-dom'
+import PropTypes from 'prop-types'
+import cn from 'classnames'
+
+import IconClose from '../Icons/Close'
+
+
+const ModalContext = React.createContext('modal')
+
+export const {
+  Provider,
+  Consumer,
+} = ModalContext
+
+
+export default class Modal extends PureComponent {
+  static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.node,
+      PropTypes.arrayOf(PropTypes.node),
+    ]).isRequired,
+    className: PropTypes.string,
+    show: PropTypes.bool,
+
+    onCloseClick: PropTypes.func,
+  }
+
+  onKeyDown = (event) => {
+    if (event.keyCode === 27) {
+      this.close('escape')(event)
+    }
+  }
+
+  close = source => (event) => {
+    const {
+      onCloseClick,
+    } = this.props
+
+    if (onCloseClick) {
+      onCloseClick(source, event)
+    }
+  }
+
+  render () {
+    const {
+      children,
+      className,
+      show,
+
+      onCloseClick,
+    } = this.props
+
+    if (!show) {
+      return null
+    }
+
+    const classes = cn({
+      [className]: className,
+      'mc-modal': true,
+    })
+
+    return createPortal(
+      <Provider value={{ close: this.close }}>
+        <div
+          className={classes}
+          onKeyDown={this.onKeyDown}
+          ref={this.container}
+        >
+          <div className='mc-modal__backdrop' />
+
+          {onCloseClick &&
+            <div
+              className='mc-modal__close'
+              onClick={this.close('close')}
+            >
+              <IconClose />
+            </div>
+          }
+
+          <div className='mc-modal__content-container'>
+            <div className='mc-modal__content-container-inner'>
+              {children}
+            </div>
+          </div>
+        </div>
+      </Provider>,
+      document.body,
+    )
+  }
+}
---
+++ src/components/ModalContent/index.js
@@ -0,0 +1,53 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import cn from 'classnames'
+
+import { Consumer } from '../Modal'
+import ClickOutside from '../ClickOutside'
+
+
+export default class ModalContent extends PureComponent {
+  static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.node,
+      PropTypes.arrayOf(PropTypes.node),
+    ]).isRequired,
+    className: PropTypes.string,
+  }
+
+  componentDidMount () {
+    this.content.current.focus()
+  }
+
+  content = React.createRef()
+
+  render () {
+    const {
+      children,
+      className,
+    } = this.props
+
+    const classes = cn({
+      [className]: className,
+      'mc-modal__content': true,
+    })
+
+    return (
+      <Consumer>
+        {({ close }) =>
+          <div
+            className={classes}
+            ref={this.content}
+            tabIndex='-1'
+          >
+            <ClickOutside
+              onClickOutside={close('backdrop')}
+            >
+              {children}
+            </ClickOutside>
+          </div>
+        }
+      </Consumer>
+    )
+  }
+}
