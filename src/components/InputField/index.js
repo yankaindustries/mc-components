12082083c153a17ece60ff05419ca520707601a4
Justin Jones <justinjones53@gmail.com>
2018-10-29 11:36:57 -0700

    Adding prop types to InputField

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Input from '../Input'
+
+
+const INPUT_PROP_TYPE = PropTypes.shape({
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.any.isRequired,
+})
+
+const META_PROP_TYPE = PropTypes.shape({
+  error: PropTypes.string,
+})
 
 
 const InputField = ({
@@ -11,11 +23,15 @@ const InputField = ({
   ...props
 }) => (
   <Input
-    {...input}
     error={error}
+    {...input}
     {...props}
   />
 )
 
+InputField.propTypes = {
+  input: INPUT_PROP_TYPE,
+  meta: META_PROP_TYPE,
+}
 
 export default InputField
