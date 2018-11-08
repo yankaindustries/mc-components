435c0ddd48b84198ea7c7d272e0410410b125f26
Justin Jones <justinjones53@gmail.com>
2018-11-07 19:45:02 -0800

    Improving step formula with decay, adding more beta typography examples

--- src/foundation/type/beta.stories.js
+++ src/foundation/type/beta.stories.js
@@ -3,6 +3,10 @@ import { storiesOf } from '@storybook/react'
 import cn from 'classnames'
 
 import Button from '../../components/Button'
+import Tile from '../../components/Tile'
+import TileImage from '../../components/TileImage'
+import AnimationHandler from '../../components/AnimationHandler'
+import HoverHandler from '../../components/HoverHandler'
 
 
 class Summary extends PureComponent {
@@ -128,9 +132,28 @@ class Summary extends PureComponent {
               h6
             </div>
             <div className='col-10'>
-              <h5 className={`mcb-text-h6 ${modifierClasses}`}>
+              <h6 className={`mcb-text-h6 ${modifierClasses}`}>
                 Account Settings
-              </h5>
+              </h6>
+            </div>
+
+            <div className='col-2 mc-text--right mc-text--muted'>
+              h7
+            </div>
+            <div className='col-10'>
+              <h6 className={`mcb-text-h7 ${modifierClasses}`}>
+                Account Settings
+              </h6>
+            </div>
+
+            <div className='col-2 mc-text--right mc-text--muted'>
+              large
+            </div>
+            <div className='col-10'>
+              <p className='mcb-text-large'>
+                Online classes taught by the world&#39;s greatest minds.<br />
+                Now get unlimited access to all classes.
+              </p>
             </div>
 
             <div className='col-2 mc-text--right mc-text--muted'>
@@ -138,6 +161,16 @@ class Summary extends PureComponent {
             </div>
             <div className='col-10'>
               <p>
+                Online classes taught by the world&#39;s greatest minds.<br />
+                Now get unlimited access to all classes.
+              </p>
+            </div>
+
+            <div className='col-2 mc-text--right mc-text--muted'>
+              small
+            </div>
+            <div className='col-10'>
+              <p className='mcb-text-small'>
                 Online classes taught by the world&#39;s greatest minds.<br />
                 Now get unlimited access to all classes.
               </p>
@@ -158,104 +191,162 @@ class Summary extends PureComponent {
                 </h4>
               </div>
             </div>
+          </div>
 
-            <div className='row'>
-              <div className='col-sm-6 offset-sm-1'>
-                <div className='mcb-space-9'>
-                  <h3 className='mcb-text-h3 mc-text--uppercase'>
-                    Group Workshop: From Here To Alli By Corey Wright
-                  </h3>
-                  <p className='mcb-text-intro mc-text--muted mcb-space-2'>
-                    Every great story is born from intentions and obstacles.
-                    Learn how to build the &quot;drive shaft&quot; that will
-                    set your script in motion.
+          <div className='row'>
+            <div className='col-sm-6 offset-sm-1'>
+              <div className='mcb-space-9'>
+                <h3 className='mcb-text-h3 mc-text--uppercase mcb-space-2'>
+                  Group Workshop: From Here To Alli By Corey Wright
+                </h3>
+                <p className='mcb-text-intro mc-text--muted mcb-space-2'>
+                  Every great story is born from intentions and obstacles.
+                  Learn how to build the &quot;drive shaft&quot; that will
+                  set your script in motion.
+                </p>
+                <h6 className='mcb-text-small mc-text--silenced mc-text--uppercase mc-text--normal mcb-space-3'>
+                  Lesson 16 // 5min 40s
+                </h6>
+              </div>
+
+              <div className='mcb-space-9'>
+                <h3 className='mcb-text-h5 mcb-space-2'>
+                  Group Workshop: From Here To Alli By Corey Wright
+                </h3>
+                <p className='mc-text--muted mcb-space-2'>
+                  Every great story is born from intentions and obstacles.
+                  Learn how to build the &quot;drive shaft&quot; that will
+                  set your script in motion.
+                </p>
+                <h6 className='mcb-text-small mc-text--silenced mc-text--uppercase mc-text--normal mcb-space-3'>
+                  Lesson 16 // 5min 40s
+                </h6>
+              </div>
+
+              <div className='mcb-space-9'>
+                <h3 className='mcb-text-h5 mcb-space-2'>
+                  Group Workshop: From Here To Alli By Corey Wright
+                </h3>
+                <p className='mc-text--muted mcb-space-2'>
+                  Every great story is born from intentions and obstacles.
+                  Learn how to build the &quot;drive shaft&quot; that will
+                  set your script in motion.
+                </p>
+                <h6 className='mcb-text-small mc-text--silenced mc-text--uppercase mc-text--normal mcb-space-3'>
+                  Lesson 16 // 5min 40s
+                </h6>
+              </div>
+            </div>
+
+            <div className='col-sm-4'>
+              <div className='row'>
+                <div className='col-auto'>
+                  <p className='mc-text--muted'>
+                    3
                   </p>
-                  <h6 className='mcb-text-h6 mc-text--silenced mc-text--uppercase mc-text--normal mcb-space-3'>
-                    Lesson 16 // 5min 40s
-                  </h6>
                 </div>
 
-                <div className='mcb-space-9'>
-                  <h3 className='mcb-text-h5'>
-                    Group Workshop: From Here To Alli By Corey Wright
+                <div className='col-10'>
+                  <h3 className='mcb-text-h7 mc-text--muted mc-text--uppercase mcb-space-2'>
+                    Up Next
                   </h3>
-                  <p className='mc-text--muted mcb-space-2'>
-                    Every great story is born from intentions and obstacles.
-                    Learn how to build the &quot;drive shaft&quot; that will
-                    set your script in motion.
-                  </p>
-                  <h6 className='mcb-text-h6 mc-text--silenced mc-text--uppercase mc-text--normal mcb-space-3'>
-                    Lesson 16 // 5min 40s
-                  </h6>
-                </div>
 
-                <div className='mcb-space-9'>
-                  <h3 className='mcb-text-h5'>
-                    Group Workshop: From Here To Alli By Corey Wright
-                  </h3>
-                  <p className='mc-text--muted mcb-space-2'>
-                    Every great story is born from intentions and obstacles.
-                    Learn how to build the &quot;drive shaft&quot; that will
-                    set your script in motion.
-                  </p>
-                  <h6 className='mcb-text-h6 mc-text--silenced mc-text--uppercase mc-text--normal mcb-space-3'>
-                    Lesson 16 // 5min 40s
+                  <h6 className='mcb-text-h6 mcb-space-2'>
+                    Mastering Ingredients: Vegetables &amp; Herbs
                   </h6>
+                  <p className='mcb-text-small mc-text--silenced'>
+                    Do ugly vegetables taste better? Which are the most
+                    versatile herbs? Gordon shows you how to select great
+                    produce to create phenomenal dishes.
+                  </p>
                 </div>
               </div>
 
-              <div className='col-sm-4'>
-                <div className='mcb-space-9'>
-                  <h3 className='mcb-text-h7 mc-text--muted mc-text--uppercase'>
-                    Up Next
-                  </h3>
+              <hr />
+
+              <div className='row'>
+                <div className='col-auto'>
+                  <p className='mc-text--muted'>
+                    4
+                  </p>
                 </div>
 
-                <div className='mcb-space-3'>
-                  <div className='row'>
-                    <div className='col-auto'>
-                      <p className='mc-text--muted'>
-                        4
-                      </p>
-                    </div>
+                <div className='col-10'>
+                  <h6 className='mcb-text-h6 mcb-space-2'>
+                    Mastering Ingredients: Vegetables &amp; Herbs
+                  </h6>
+                  <p className='mcb-text-small mc-text--silenced'>
+                    Do ugly vegetables taste better? Which are the most
+                    versatile herbs? Gordon shows you how to select great
+                    produce to create phenomenal dishes.
+                  </p>
+                </div>
+              </div>
 
-                    <div className='col-10'>
-                      <h6 className='mcb-text-h6'>
-                        Mastering Ingredients: Vegetables &amp; Herbs
-                      </h6>
-                      <p className='mc-text--silenced mcb-space-2'>
-                        Do ugly vegetables taste better? Which are the most
-                        versatile herbs? Gordon shows you how to select great
-                        produce to create phenomenal dishes.
-                      </p>
-                    </div>
-                  </div>
+              <hr />
+
+              <div className='row'>
+                <div className='col-auto'>
+                  <p className='mc-text--muted'>
+                    5
+                  </p>
                 </div>
 
-                <hr />
-
-                <div className='mcb-space-3'>
-                  <div className='row'>
-                    <div className='col-auto'>
-                      <p className='mc-text--muted'>
-                        4
-                      </p>
-                    </div>
-
-                    <div className='col-10'>
-                      <h6 className='mcb-text-h6'>
-                        Mastering Ingredients: Vegetables &amp; Herbs
-                      </h6>
-                      <p className='mc-text--silenced mcb-space-2'>
-                        Do ugly vegetables taste better? Which are the most
-                        versatile herbs? Gordon shows you how to select great
-                        produce to create phenomenal dishes.
-                      </p>
-                    </div>
-                  </div>
+                <div className='col-10'>
+                  <h6 className='mcb-text-h6 mcb-space-2'>
+                    Mastering Ingredients: Vegetables &amp; Herbs
+                  </h6>
+                  <p className='mcb-text-small mc-text--silenced'>
+                    Do ugly vegetables taste better? Which are the most
+                    versatile herbs? Gordon shows you how to select great
+                    produce to create phenomenal dishes.
+                  </p>
                 </div>
               </div>
             </div>
+          </div>
+
+          <hr />
+
+          <div className='row'>
+            {[1, 2, 3].map(key =>
+              <div key={key} className='col-sm-6 col-lg-4'>
+                <HoverHandler>
+                  {({ hovering }) =>
+                    <AnimationHandler
+                      type='zoom'
+                      animating={hovering}
+                    >
+                      <div>
+                        <Tile>
+                          <TileImage imageUrl='https://placekitten.com/800/800' />
+                        </Tile>
+
+                        <div className='mc-card mc-background mc-background--dark'>
+                          <h6 className='mcb-text-h7 mc-text--muted mc-text--uppercase mcb-space-2'>
+                            Building Your Home Studio
+                          </h6>
+
+                          <h4 className='mcb-text-h4 mcb-space-2'>
+                            Margaret Atwood
+                          </h4>
+
+                          <p className='mc-text--muted mcb-space-2'>
+                            Mixtape tumblr chartreuse snackwave 8-bit
+                            selfies, glossier mumblecore fingerstache church-key
+                            kombucha. Hot chocolate.
+                          </p>
+
+                          <p className='mcb-text-small mc-text--silenced'>
+                            12 Lessons • Music
+                          </p>
+                        </div>
+                      </div>
+                    </AnimationHandler>
+                  }
+                </HoverHandler>
+              </div>,
+            )}
           </div>
         </div>
       </div>
