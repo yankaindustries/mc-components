yankaindustries/mc-components

commit 52ecd08f7d7e952545775ec773abd9b9fca85654

Improving documentation for <VideoPlayer /> (#330)
* Improving documentation for <VideoPlayer />

* Expanding events

* revert

src/components/VideoPlayer/index.js
@@ -8,29 +8,28 @@ import { renderChildren } from '../helpers'
 const VOLUME_INTERVAL = 0.05
 const SKIP_INTERVAL = 5
 
+const SCREEN_BEFORE = 'SCREEN_BEFORE'
+const SCREEN_END = 'SCREEN_END'
+const SCREEN_PAUSE = 'SCREEN_PAUSE'
+const SCREEN_NONE = 'SCREEN_NONE'
+
 export default class VideoPlayer extends PureComponent {
   static propTypes = {
+    accountId: PropTypes.string,
     playerId: PropTypes.string.isRequired,
     videoId: PropTypes.string.isRequired,
-    /** Pass in a react component to be shown at the end of the video. */
+
     endscreenComponent: PropTypes.func,
-    /** Pass in a react component to be shown before video starts. */
     beforescreenComponent: PropTypes.func,
-    /** Pass in a react component to be shown when the video is paused. */
     pausescreenComponent: PropTypes.func,
-    /**
-     * Keeps video constrained to standard sizes, eg 1080x720.
-     * Used to keep videos looking crisp.
-     * Scales responsively.
-     * */
+
+    hasAutoplay: PropTypes.bool,
     hasBreakpoints: PropTypes.bool,
     hasControls: PropTypes.bool,
-    isMuted: PropTypes.bool,
     isLooped: PropTypes.bool,
-    hasAutoplay: PropTypes.bool,
-    /** Player has been initialized */
+    isMuted: PropTypes.bool,
+
     onPlayerReady: PropTypes.func,
-    /** Video is loaded and ready to play */
     onVideoReady: PropTypes.func,
     onPlay: PropTypes.func,
     onPause: PropTypes.func,
@@ -38,28 +37,26 @@ export default class VideoPlayer extends PureComponent {
     onTimeChange: PropTypes.func,
     onError: PropTypes.func,
     onSeek: PropTypes.func,
-    accountId: PropTypes.string,
     progress: PropTypes.number,
   }
 
   static defaultProps = {
+    accountId: '5344802162001',
     playerId: 'rkcQq7gAe',
     videoId: '5450137526001',
-    isLooped: false,
-    isMuted: false,
+
     hasAutoplay: true,
-    hasControls: true,
     hasBreakpoints: false,
-    accountId: '5344802162001',
+    hasControls: true,
+    isMuted: false,
+    isLooped: false,
   }
 
   playerRef = React.createRef()
   currentTime = 0
 
   state = {
-    endscreenOpen: false,
-    beforescreenOpen: false,
-    pausescreenOpen: false,
+    screen: SCREEN_NONE,
   }
 
   componentDidMount () {
@@ -75,33 +72,24 @@ export default class VideoPlayer extends PureComponent {
       // Call a function to play the video once player's JavaScropt loaded
       bcScript.onload = this.setupVideo
     }
-    const { progress, beforescreenComponent } = this.props
+
+    const {
+      progress,
+      beforescreenComponent,
+    } = this.props
 
     if (progress) {
       this.playerRef.current.currentTime = progress
     }
 
     if (beforescreenComponent) {
-      this.setState({ beforescreenOpen: true })
+      this.setState({ screen: SCREEN_BEFORE })
     }
   }
 
   componentWillReceiveProps (nextProps) {
     if (this.props.videoId !== nextProps.videoId) {
       this.replaceWith(nextProps.videoId)
-    }
-  }
-
-  hideOpenScreens = () => {
-    const { beforescreenOpen, pausescreenOpen, endscreenOpen } = this.state
-    if (beforescreenOpen) {
-      this.setState({ beforescreenOpen: false })
-    }
-    if (pausescreenOpen) {
-      this.setState({ pausescreenOpen: false })
-    }
-    if (endscreenOpen) {
-      this.setState({ endscreenOpen: false })
     }
   }
 
@@ -116,18 +104,20 @@ export default class VideoPlayer extends PureComponent {
     } = this.props
 
     this.video.on('play', () => {
-      this.hideOpenScreens()
       if (onPlay) {
         onPlay(this.video)
       }
+
+      this.setState({ screen: SCREEN_NONE })
     })
 
     this.video.on('pause', () => {
-      if (pausescreenComponent) {
-        this.setState({ pausescreenOpen: true })
-      }
       if (onPause) {
         onPause(this.video)
+      }
+
+      if (pausescreenComponent) {
+        this.setState({ screen: SCREEN_PAUSE })
       }
     })
 
@@ -156,20 +146,19 @@ export default class VideoPlayer extends PureComponent {
   handleVideoEnd = () => {
     this.currentTime = 0
     this.hasEnded = true
+
     const {
       isLooped,
       endscreenComponent,
-      pausescreenComponent,
       onEnd,
     } = this.props
+
     if (isLooped) {
       this.video.play()
     } else if (endscreenComponent) {
-      this.setState({ endscreenOpen: true })
+      this.setState({ screen: SCREEN_END })
     }
-    if (pausescreenComponent) {
-      this.setState({ pausescreenOpen: false })
-    }
+
     if (onEnd) {
       onEnd(this.video)
     }
@@ -177,9 +166,8 @@ export default class VideoPlayer extends PureComponent {
 
   handleReplayClick = () => {
     this.video.play()
-    this.setState({
-      endscreenOpen: false,
-    })
+
+    this.setState({ screen: SCREEN_NONE })
   }
 
   resumeVideo = () => { this.video.play() }
@@ -278,7 +266,7 @@ export default class VideoPlayer extends PureComponent {
       this.hasEnded = false
       this.currentTime = 0
       this.setState({
-        endscreenOpen: false,
+        screen: SCREEN_NONE,
       })
       this.video.play()
     })
@@ -336,68 +324,76 @@ export default class VideoPlayer extends PureComponent {
       isMuted,
       accountId,
     } = this.props
+
     const {
-      endscreenOpen,
-      beforescreenOpen,
-      pausescreenOpen,
+      screen,
     } = this.state
-    const isScreenOpen = endscreenOpen || beforescreenOpen || pausescreenOpen
+
+    const isScreenOpen = screen !== SCREEN_NONE
+
+    const containerClasses = cn('bc-player', {
+      'bc-player--screen-open': isScreenOpen,
+      'bc-player--has-breakpoints': hasBreakpoints,
+    })
+
+    const playerClasses = cn(
+      'video-js',
+      'bc-player__video',
+      'bc-player__video--default',
+    )
+
     // eslint-disable-next-line
     const videoRoot = this.video ? this.video.el_ : undefined
 
     return (
       <div
-        className={cn('bc-player', {
-          'bc-player--screen-open': isScreenOpen,
-          'bc-player--has-breakpoints': hasBreakpoints,
-        })}
+        className={containerClasses}
         onKeyDown={this.handleKeyDown}
       >
         {endscreenComponent && videoRoot &&
           <VideoPlayerScreen
-            isActive={endscreenOpen}
+            isActive={screen === SCREEN_END}
             variation='endscreen'
             videoRoot={videoRoot}
           >
-            {renderChildren(
-              endscreenComponent,
-              { onReplay: this.handleReplayClick, isActive: endscreenOpen })
-            }
+            {renderChildren(endscreenComponent, {
+              onReplay: this.handleReplayClick,
+              isActive: screen === SCREEN_END,
+            })}
           </VideoPlayerScreen>
         }
+
         {beforescreenComponent && videoRoot &&
           <VideoPlayerScreen
-            isActive={beforescreenOpen}
+            isActive={screen === SCREEN_BEFORE}
             variation='beforescreen'
             videoRoot={videoRoot}
           >
-            {renderChildren(
-              beforescreenComponent,
-              { onResume: this.resumeVideo, isActive: beforescreenOpen })
-            }
+            {renderChildren(beforescreenComponent, {
+              onResume: this.resumeVideo,
+              isActive: screen === SCREEN_BEFORE,
+            })}
           </VideoPlayerScreen>
         }
+
         {pausescreenComponent && videoRoot &&
           <VideoPlayerScreen
-            isActive={pausescreenOpen}
+            isActive={screen === SCREEN_PAUSE}
             variation='pausescreen'
             videoRoot={videoRoot}
           >
-            {renderChildren(
-              pausescreenComponent,
-              { onResume: this.resumeVideo, isActive: pausescreenOpen })
-            }
+            {renderChildren(pausescreenComponent, {
+              onResume: this.resumeVideo,
+              isActive: screen === SCREEN_PAUSE,
+            })}
           </VideoPlayerScreen>
         }
+
         <div className='bc-player__wrapper'>
           <video
             data-application-id
             ref={this.playerRef}
-            className={cn(
-              'video-js',
-              'bc-player__video',
-              'bc-player__video--default',
-            )}
+            className={playerClasses}
             data-embed='default'
             data-video-id={videoId}
             data-player-id={playerId}

src/components/ImageTile/index.js
@@ -79,7 +79,8 @@ export default class ImageTile extends PureComponent {
     return (
       <div
       className={classNames}
-      style={style}>
+      style={style}
+      >
         <div
           className={'background'}
           style={this.backgroundStyle(imageUrl)}

src/components/AnimationHandler/index.stories.js
@@ -64,7 +64,7 @@ storiesOf('utilities|AnimationHandler', module)
         </PropExample>
       </DocSection>
 
-      <DocSection title='Example'>
+      <DocSection title='Demo'>
         <PropExample>
           <div style={{ maxWidth: '500px' }}>
             <HoverHandler>

src/components/Background/index.stories.js
@@ -34,7 +34,7 @@ storiesOf('components|Background', module)
           </div>
         </div>
 
-        <DocSection title='Examples'>
+        <DocSection title='Demo'>
           <CodeExample>
             <Background
               element={<img src={thomasKellerBackground} />}

src/components/ResponsiveHandler/index.stories.js
@@ -23,7 +23,7 @@ storiesOf('utilities|ResponsiveHandler', module)
         </p>
       </div>
 
-      <DocSection title='Example'>
+      <DocSection title='Demo'>
         <PropExample>
           <div className='row row--vertcal-space'>
             <div className='col-12'>

src/components/VideoPlayer/index.stories.js
@@ -1,10 +1,22 @@
 import React, { Fragment } from 'react'
-import { func } from 'prop-types'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import { withProps } from '../../utils/addon-props'
 
+import DocHeader from '../../utils/DocHeader'
+import DocSection from '../../utils/DocSection'
+import PropExample from '../../utils/PropExample'
+
 import VideoPlayer from './'
-import Replay from '../Icons/Replay'
+import Tile from '../Tile'
+import TileCaption from '../TileCaption'
+import TileImage from '../TileImage'
+import TileVideo from '../TileVideo'
+import HoverHandler from '../HoverHandler'
+import AnimationHandler from '../AnimationHandler'
+
+import shondaRhimesThumbnail from '../../utils/shonda-rhimes.png'
+
 
 const screenStyle = {
   width: '100%',
@@ -15,75 +27,311 @@ const screenStyle = {
   flexDirection: 'column',
 }
 
-const replayIconStyle = {
-  position: 'absolute',
-  bottom: '24px',
-  left: '24px',
-  zIndex: '1',
-  width: '48px',
-  height: '48px',
-  opacity: '0.8',
-  fill: 'white',
-  cursor: 'pointer',
-}
-
-const InitialPlayerScreen = ({ onResume }) =>
-  <div style={screenStyle}>
-    <p>Start playing your video</p>
-    <button onClick={onResume}>
-      START
-    </button>
-  </div>
-
-InitialPlayerScreen.propTypes = {
-  onResume: func,
-}
-
-const PausePlayerScreen = ({ onResume }) =>
-  <div style={screenStyle}>
-    <p>Your video is paused</p>
-    <button onClick={onResume}>
-      CONTINUE
-    </button>
-  </div>
-
-PausePlayerScreen.propTypes = {
-  onResume: func,
-}
-
-const EndPlayerScreen = ({ onReplay }) => (
-  <Fragment>
-    <Replay
-      style={replayIconStyle}
-      width='100%'
-      onClick={onReplay}
-    />
-    <div style={screenStyle}>
-      <p>END SCREEN</p>
-    </div>
-  </Fragment>
-)
-
-EndPlayerScreen.propTypes = {
-  onReplay: func,
-}
 
 storiesOf('components|VideoPlayer', module)
-  .add('default', withProps(VideoPlayer)(() =>
-    <VideoPlayer />,
+  .add('Summary', withProps(VideoPlayer)(() =>
+    <div className='container'>
+      <DocHeader
+        title='VideoPlayer'
+        description='A dynamic and simple to set up video player.'
+      />
+
+      <DocSection title='Demo'>
+        <div className='row'>
+          <div className='col-12'>
+            <VideoPlayer
+              hasAutoplay={false}
+              beforescreenComponent={
+                ({ onResume }) =>
+                  <div style={screenStyle}>
+                    <p>Start playing your video</p>
+                    <button onClick={onResume}>
+                      START
+                    </button>
+                  </div>
+              }
+              pausescreenComponent={
+                ({ onResume }) =>
+                  <div style={screenStyle}>
+                    <p>Your video is paused</p>
+                    <button onClick={onResume}>
+                      CONTINUE
+                    </button>
+                  </div>
+              }
+              endscreenComponent={
+                ({ onReplay }) =>
+                  <div style={screenStyle}>
+                    <p>Your video has ended</p>
+                    <button onClick={onReplay}>
+                      REPLAY
+                    </button>
+                  </div>
+              }
+            />
+          </div>
+
+          {[1, 2, 3].map(key =>
+            <div className='col-sm-4' key={key}>
+              <HoverHandler>
+                {({ intent }) =>
+                  <AnimationHandler type='zoom' animating={intent}>
+                    <Tile>
+                      <Fragment>
+                        <TileImage imageUrl={shondaRhimesThumbnail} />
+                        <TileCaption>
+                          <h5 className='mc-text-h5 mc-text--uppercase'>
+                            Shonda Rhimes
+                          </h5>
+                          <p className='mc-text--muted'>
+                            Teaches Writing For Television
+                          </p>
+                        </TileCaption>
+                      </Fragment>
+
+                      {intent &&
+                        <TileVideo />
+                      }
+                    </Tile>
+                  </AnimationHandler>
+                }
+              </HoverHandler>
+            </div>,
+          )}
+        </div>
+      </DocSection>
+
+      <DocSection title='Properties'>
+        <PropExample
+          name='accountId | playerId | videoId'
+          type='string'
+          description='
+            The default values for these are for the MasterClass account
+            of the Brightcove service.
+          '
+        >
+          <div className='row'>
+            <div className='col-lg-4'>
+              <VideoPlayer hasAutoplay={false} />
+            </div>
+          </div>
+        </PropExample>
+
+        <div className='row'>
+          <div className='col-lg-4'>
+            <PropExample
+              name='beforeScreenComponent'
+              type='function({ onResume, isActive })'
+              description='Renders a component before the video plays.'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                beforescreenComponent={
+                  ({ onResume }) =>
+                    <div style={screenStyle}>
+                      <p>Start playing your video</p>
+                      <button onClick={onResume}>
+                        START
+                      </button>
+                    </div>
+                }
+              />
+            </PropExample>
+          </div>
+
+          <div className='col-lg-4'>
+            <PropExample
+              name='pausescreenComponent'
+              type='function({ onResume, isActive })'
+              description='Renders a component when the video is paused.'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                pausescreenComponent={
+                  ({ onResume }) =>
+                    <div style={screenStyle}>
+                      <p>Your video is paused</p>
+                      <button onClick={onResume}>
+                        CONTINUE
+                      </button>
+                    </div>
+                }
+              />
+            </PropExample>
+          </div>
+
+          <div className='col-lg-4'>
+            <PropExample
+              name='endscreenComponent'
+              type='function({ onReplay, isActive })'
+              description='Renders a component when the video is done.'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                progress={11}
+                endscreenComponent={
+                  ({ onReplay }) =>
+                    <div style={screenStyle}>
+                      <p>Your video has ended</p>
+                      <button onClick={onReplay}>
+                        REPLAY
+                      </button>
+                    </div>
+                }
+              />
+            </PropExample>
+          </div>
+        </div>
+
+        <PropExample
+          name='hasAutoplay'
+          type='boolean'
+          description='Automatically plays the video when ready.'
+        >
+          <div className='row'>
+            <div className='col-lg-4'>
+              <VideoPlayer hasAutoplay={true} />
+            </div>
+          </div>
+        </PropExample>
+
+        <PropExample
+          name='isLooped'
+          type='boolean'
+          description='
+            Automatically replays the video when it has reached the end.
+          '
+        >
+          <div className='row'>
+            <div className='col-lg-4'>
+              <VideoPlayer
+                hasAutoplay={false}
+                isLooped={true}
+              />
+            </div>
+          </div>
+        </PropExample>
+
+        <PropExample
+          name='isMuted'
+          type='boolean'
+          description='Defaults the videos volume to zero.'
+        >
+          <div className='row'>
+            <div className='col-lg-4'>
+              <VideoPlayer
+                hasAutoplay={false}
+                isMuted={true}
+              />
+            </div>
+          </div>
+        </PropExample>
+
+        <div className='row'>
+          <div className='col-lg-4'>
+            <PropExample
+              name='onPlayerReady'
+              type='function(video)'
+              description='Callback event'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                onPlayerReady={action('onPlayerReady')}
+              />
+            </PropExample>
+          </div>
+
+          <div className='col-lg-4'>
+            <PropExample
+              name='onVideoReady'
+              type='function(video)'
+              description='Callback event'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                onVideoReady={action('onVideoReady')}
+              />
+            </PropExample>
+          </div>
+
+          <div className='col-lg-4'>
+            <PropExample
+              name='onPlay'
+              type='function(video)'
+              description='Callback event'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                onPlay={action('onPlay')}
+              />
+            </PropExample>
+          </div>
+
+          <div className='col-lg-4'>
+            <PropExample
+              name='onPause'
+              type='function(video)'
+              description='Callback event'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                onPause={action('onPause')}
+              />
+            </PropExample>
+          </div>
+
+          <div className='col-lg-4'>
+            <PropExample
+              name='onEnd'
+              type='function(video)'
+              description='Callback event'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                onEnd={action('onEnd')}
+              />
+            </PropExample>
+          </div>
+
+          <div className='col-lg-4'>
+            <PropExample
+              name='onTimeChange'
+              type='function(currentTime, remainingTime)'
+              description='Callback event'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                onTimeChange={action('onTimeChange')}
+              />
+            </PropExample>
+          </div>
+
+          <div className='col-lg-4'>
+            <PropExample
+              name='onError'
+              type='function(error, video)'
+              description='Callback event'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                onError={action('onError')}
+              />
+            </PropExample>
+          </div>
+
+          <div className='col-lg-4'>
+            <PropExample
+              name='onSeek'
+              type='function(video)'
+              description='Callback event'
+            >
+              <VideoPlayer
+                hasAutoplay={false}
+                onSeek={action('onSeek')}
+              />
+            </PropExample>
+          </div>
+        </div>
+      </DocSection>
+    </div>,
   ))
-  .add('ambient video', withProps(VideoPlayer)(() => (
-    <VideoPlayer
-      isMuted
-      isLooped
-      hasControls={false}
-    />
-  )))
-  .add('overlay screens', withProps(VideoPlayer)(() => (
-    <VideoPlayer
-      hasAutoplay={false}
-      beforescreenComponent={InitialPlayerScreen}
-      pausescreenComponent={PausePlayerScreen}
-      endscreenComponent={EndPlayerScreen}
-    />
-  )))

src/utils/PropExample.js
@@ -22,17 +22,16 @@ export default class Definition extends PureComponent {
 
     return (
       <div className='example__definition'>
-        <p>
-          <span className='example__definition-name mc-text--bold'>
-            {name}
-          </span>
-          {' '}
-          {type &&
-            <span className='example__definition-type mc-text--monospace'>
-              &lt;{type}&gt;
-            </span>
-          }
+        <p className='example__definition-name mc-text--bold'>
+          {name}
         </p>
+        {' '}
+
+        {type &&
+          <p className='example__definition-type mc-text--monospace mc-text--nowrap'>
+            &lt;{type}&gt;
+          </p>
+        }
 
         {description &&
           <p className='example__definition-description mc-text--muted'>

commit 4e7f293109e759fda6c0739bd4ae6b0393549583

Video Player: fix onseek event (#350)
## Overview
onseek event isn't working because it's calling a undefined function.

Bug introduced in #330 

## Risks
None

src/components/VideoPlayer/index.js
@@ -124,7 +124,7 @@ export default class VideoPlayer extends PureComponent {
     this.video.on('ended', this.handleVideoEnd)
 
     this.video.on('seeking', () => {
-      this.hideOpenScreens()
+      this.setState({ screen: SCREEN_NONE })
       if (onSeek) {
         onSeek(this.video)
       }