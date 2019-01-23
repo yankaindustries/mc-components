1e1d9511ff501788493d64f0b2a824f9964cd611
Stephen Pontes <stephen@shibbay.com>
2019-01-23 15:19:49 -0800

    Remove the facebook and twitter custom link colors

--- src/foundation/typography/index.stories.js
+++ src/foundation/typography/index.stories.js
@@ -5,11 +5,9 @@ import Summary from './summary.stories'
 import Modifiers from './modifiers.stories'
 import Alignment from './alignment.stories'
 import Flourishes from './flourishes.stories'
-import Colors from './colors.stories'
 
 storiesOf('Foundation|Typography', module)
   .add('Summary', () => <Summary />)
   .add('Modifiers', () => <Modifiers />)
   .add('Alignment', () => <Alignment />)
   .add('Flourishes', () => <Flourishes />)
-  .add('Colors', () => <Colors />)
