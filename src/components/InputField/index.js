b6f6b792b8397bf6dbc06d21cab00d62663197a3
Bernabe Gonzalez <bernabe@rootstrap.com>
2019-02-08 12:54:52 -0300

    Input: Allow array of errors

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -11,9 +11,18 @@ const INPUT_PROP_TYPE = PropTypes.shape({
 })
 
 const META_PROP_TYPE = PropTypes.shape({
-  error: PropTypes.string,
+  error: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]),
 })
 
+const parseError = (error) => {
+  if (!Array.isArray(error)) {
+    return error
+  }
+  return error.length > 0 ? error[0] : undefined
+}
 
 const InputField = ({
   input,
@@ -24,7 +33,7 @@ const InputField = ({
   ...props
 }) => (
   <Input
-    error={error}
+    error={parseError(error)}
     touched={touched}
     {...input}
     {...props}
