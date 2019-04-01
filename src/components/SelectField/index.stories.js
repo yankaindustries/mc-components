1a42f8c3986362977a271c01048d841e28fdac9b
Stephen Pontes <stephen@shibbay.com>
2019-04-01 12:27:44 -0700

    Add documentation for select fields and clean up disabled form states

---
+++ src/components/SelectField/index.stories.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import {
+  reducer as formReducer,
+  reduxForm,
+  Field,
+} from 'redux-form'
+import { storiesOf } from '@storybook/react'
+
+import DocHeader from '../../utils/DocHeader'
+import InvertedMirror from '../../utils/InvertedMirror'
+import SelectField from '../SelectField'
+
+
+const reducer = combineReducers({ form: formReducer })
+const store = createStore(reducer)
+
+const membershipOptions = [
+  {
+    label: 'Silver',
+    value: '1',
+  },
+  {
+    label: 'Gold',
+    value: '2',
+  },
+  {
+    label: 'Platinum',
+    value: '3',
+  },
+]
+
+const Form = reduxForm({
+  form: 'select',
+  initialValues: {
+    disabledChecked: '1',
+  },
+})(
+  () =>
+    <div className='container'>
+      <DocHeader
+        title='SelectField'
+        description='I agree to the terms...'
+      />
+
+      <InvertedMirror>
+        <Field
+          component={SelectField}
+          name='memberships'
+          label='Membership Options'
+          options={membershipOptions}
+        />
+
+        <Field
+          component={SelectField}
+          name='disabled'
+          disabled
+          label='
+            This select dropdown is disabled
+          '
+        />
+
+        <Field
+          component={SelectField}
+          name='disabledChecked'
+          disabled
+          options={membershipOptions}
+          value='Silver'
+          label='
+            This checkbox is disabled but has a selected option
+          '
+        />
+      </InvertedMirror>
+    </div>,
+)
+
+
+storiesOf('Components|Forms/SelectField', module)
+  .add('SelectField', () => (
+    <Provider store={store}>
+      <Form />
+    </Provider>
+  ))
