d8300dffe5291de1f74158126cbc1b383e3eddea
Justin Jones <justinjones53@gmail.com>
2019-04-04 13:06:19 -0700

    fixing direction values for carousel (#448)
    ## Overview
    Set new requirements for Carousel Arrow direction, but didn't update the implementation.  This fixes that.
    
    ## Risks
    None

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -150,7 +150,7 @@ export default class Carousel extends PureComponent {
         arrows: true,
         prevArrow: (
           <Arrow
-            direction='left'
+            direction='prev'
             onPress={this.handlePrevClick}
           >
             <ChevronLeft />
@@ -158,7 +158,7 @@ export default class Carousel extends PureComponent {
         ),
         nextArrow: (
           <Arrow
-            direction='right'
+            direction='next'
             onPress={this.handleNextClick}
           >
             <ChevronRight />
