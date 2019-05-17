7bae5e2fa62619351f31c78da5d443fca3884bea
Justin Jones <justinjones53@gmail.com>
2019-05-17 10:31:27 -0700

    Tooltip Component (#485)
    * Adding Tooltip component
    
    * Adding Tooltip component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,96 +1,89 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, PureComponent } from 'react'
 import cn from 'classnames'
 import PropTypes from 'prop-types'
 
 import Loader from '../Icons/Loader'
 
 
-const Button = ({
-  children,
-  className,
-  onClick,
-  secondary,
-  tertiary,
-  fullWidth,
-  link,
-  loading,
-  symmetrical,
-  rounded,
-  kind,
-  ...props
-}) => {
-  const classNames = cn({
-    [className]: Boolean(className),
-    'c-button': true,
-    'c-button--secondary': secondary,
-    'c-button--tertiary': tertiary,
-    'c-button--full-width': fullWidth,
-    'c-button--link': link,
-    'c-button--loading': loading,
-    'c-button--symmetrical': symmetrical,
-    'c-button--symmetrical mc-corners--circle': rounded,
-    [`c-button--${kind}`]: kind,
-  })
+export default class Button extends PureComponent {
+  static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node,
+      PropTypes.string,
+    ]),
+    className: PropTypes.string,
+    fullWidth: PropTypes.bool,
+    kind: PropTypes.oneOf([
+      'applepay',
+      'facebook',
+      'google',
+      'link',
+      'paypal',
+      'pinterest',
+      'primary',
+      'secondary',
+      'success',
+      'tertiary',
+      'twitter',
+    ]),
+    link: PropTypes.bool,
+    loading: PropTypes.bool,
+    onClick: PropTypes.func,
+    secondary: PropTypes.bool,
+    symmetrical: PropTypes.bool,
+    tertiary: PropTypes.bool,
+  }
 
-  return (
-    <button
-      className={classNames}
-      onClick={onClick}
-      {...props}
-    >
-      {!loading &&
-        children
-      }
-      {loading &&
-        <Fragment>
-          <span className='c-button__content'>
-            {children}
-          </span>
-          <Loader className='c-button__loader' />
-        </Fragment>
-      }
-    </button>
-  )
+  static defaultProps = {
+    kind: 'primary',
+  }
+
+  render () {
+    const {
+      children,
+      className,
+      secondary,
+      tertiary,
+      fullWidth,
+      link,
+      loading,
+      symmetrical,
+      rounded,
+      kind,
+      ...restProps
+    } = this.props
+
+    const classNames = cn({
+      'c-button': true,
+      'c-button--secondary': secondary,
+      'c-button--tertiary': tertiary,
+      'c-button--full-width': fullWidth,
+      'c-button--link': link,
+      'c-button--loading': loading,
+      'c-button--symmetrical': symmetrical,
+      'c-button--symmetrical mc-corners--circle': rounded,
+      [`c-button--${kind}`]: kind,
+      [className]: Boolean(className),
+    })
+
+    return (
+      <a
+        className={classNames}
+        {...restProps}
+      >
+        {!loading &&
+          children
+        }
+        {loading &&
+          <Fragment>
+            <span className='c-button__content'>
+              {children}
+            </span>
+            <Loader className='c-button__loader' />
+          </Fragment>
+        }
+      </a>
+    )
+  }
 }
-
-Button.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-    PropTypes.string,
-  ]),
-  className: PropTypes.string,
-  onClick: PropTypes.func,
-  secondary: PropTypes.bool,
-  tertiary: PropTypes.bool,
-  fullWidth: PropTypes.bool,
-  link: PropTypes.bool,
-  loading: PropTypes.bool,
-  symmetrical: PropTypes.bool,
-  kind: PropTypes.oneOf([
-    'primary',
-    'secondary',
-    'tertiary',
-    'link',
-    'success',
-    'facebook',
-    'twitter',
-    'google',
-    'pinterest',
-    'applepay',
-    'paypal',
-  ]),
-}
-
-Button.defaultProps = {
-  className: '',
-  onClick: () => {},
-  secondary: false,
-  tertiary: false,
-  fullWidth: false,
-  link: false,
-  loading: false,
-  kind: 'primary',
-}
-
-export default Button
