d7b19e1fbf50fe802274b9ce82748e3ef91dc7f2
Stephen Pontes <stephen@shibbay.com>
2019-06-11 15:31:09 -0700

    Adding other arrow directions

---
+++ src/components/Icons/ArrowTop.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SvgArrowTop = props => (
+  <svg width='2em' height='2em' viewBox='0 0 24 24' fill='none' {...props}>
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M5.47 12.155a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0l6 6a.75.75 0 1 1-1.06 1.06L12 6.685l-5.47 5.47a.75.75 0 0 1-1.06 0z'
+      fill='currentColor'
+    />
+    <path
+      d='M12.023 6.25v11.5'
+      stroke='currentColor'
+      strokeWidth={1.5}
+      strokeLinecap='round'
+    />
+  </svg>
+)
+
+export default SvgArrowTop
