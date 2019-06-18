f89ecc69f290deb7ba004867fc74ef0d3daaac99
Justin Jones <justinjones53@gmail.com>
2019-06-18 11:37:12 -0700

    adding helper directly to responsive handler (#510)

--- src/components/ResponsiveHandler/index.stories.js
+++ src/components/ResponsiveHandler/index.stories.js
@@ -27,35 +27,35 @@ storiesOf('utilities|ResponsiveHandler', module)
 
       <DocSection title='Demo'>
         <PropExample>
-          <div className='row row--vertcal-space'>
-            <div className='col-12'>
-              <ResponsiveHandler>
-                {({ gteMD }) =>
-                  <Tile aspectRatio={gteMD ? '519x187' : '16x9'}>
+          <ResponsiveHandler>
+            {({ responsive }) =>
+              <div className='row row--vertcal-space'>
+                <div className='col-12'>
+                  <Tile aspectRatio={responsive('1x1', '21x9', '21x9', '21x9')}>
                     <TileImage imageUrl={shondaRhimesThumbnail} />
                   </Tile>
-                }
-              </ResponsiveHandler>
-            </div>
+                </div>
 
-            <div className='col-md-4'>
-              <Tile aspectRatio='16x9'>
-                <TileImage imageUrl={shondaRhimesThumbnail} />
-              </Tile>
-            </div>
+                <div className='col-sm-4'>
+                  <Tile aspectRatio={responsive('1x1', '16x9', '16x9', '16x9')}>
+                    <TileImage imageUrl={shondaRhimesThumbnail} />
+                  </Tile>
+                </div>
 
-            <div className='col-md-4'>
-              <Tile aspectRatio='16x9'>
-                <TileImage imageUrl={shondaRhimesThumbnail} />
-              </Tile>
-            </div>
+                <div className='col-sm-4'>
+                  <Tile aspectRatio={responsive('1x1', '16x9', '16x9', '16x9')}>
+                    <TileImage imageUrl={shondaRhimesThumbnail} />
+                  </Tile>
+                </div>
 
-            <div className='col-md-4'>
-              <Tile aspectRatio='16x9'>
-                <TileImage imageUrl={shondaRhimesThumbnail} />
-              </Tile>
-            </div>
-          </div>
+                <div className='col-sm-4'>
+                  <Tile aspectRatio={responsive('1x1', '16x9', '16x9', '16x9')}>
+                    <TileImage imageUrl={shondaRhimesThumbnail} />
+                  </Tile>
+                </div>
+              </div>
+            }
+          </ResponsiveHandler>
         </PropExample>
       </DocSection>
     </div>
