fde415634fb6f57bbb71e27afcf5bb39abc0d834
Justin Jones <justinjones53@gmail.com>
2019-06-27 11:29:45 -0700

    Adding Share and List icon (#519)
    * updating node_modules
    
    * Adding List icon

---
+++ src/components/Icons/List.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SvgList = props => (
+  <svg width='2em' height='2em' viewBox='0 0 24 24' fill='none' {...props}>
+    <path
+      d='M4 17.2a1.2 1.2 0 1 1 2.4 0 1.2 1.2 0 0 1-2.4 0zM4 11.92a1.2 1.2 0 1 1 2.4 0 1.2 1.2 0 0 1-2.4 0z'
+      fill='currentColor'
+    />
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M4 6.8a1.2 1.2 0 1 1 2.4 0 1.2 1.2 0 0 1-2.4 0zM9.6 17.2a.8.8 0 0 1 .8-.8h8.8a.8.8 0 0 1 0 1.6h-8.8a.8.8 0 0 1-.8-.8zM9.6 6.8a.8.8 0 0 1 .8-.8h8.8a.8.8 0 0 1 0 1.6h-8.8a.8.8 0 0 1-.8-.8zM9.6 12a.8.8 0 0 1 .8-.8h8.8a.8.8 0 0 1 0 1.6h-8.8a.8.8 0 0 1-.8-.8z'
+      fill='currentColor'
+    />
+  </svg>
+)
+
+export default SvgList
