e596a284caf0f4653e32e964de84757eed464285
Stephen Pontes <stephen@shibbay.com>
2019-07-22 15:41:13 -0700

    Adding play-stroke icon type

---
+++ src/components/Icons/PlayStroke.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SvgPlayStroke = props => (
+  <svg width='2em' height='2em' viewBox='0 0 24 24' fill='none' {...props}>
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M0 12C0 5.373 5.373 0 12 0s12 5.373 12 12-5.373 12-12 12S0 18.627 0 12zM12 .96C5.903.96.96 5.903.96 12c0 6.097 4.943 11.04 11.04 11.04 6.097 0 11.04-4.943 11.04-11.04C23.04 5.903 18.097.96 12 .96z'
+      fill='currentColor'
+    />
+    <path
+      fillRule='evenodd'
+      clipRule='evenodd'
+      d='M16.294 11.774l-5.67-3.679a.634.634 0 0 0-.57-.05c-.186.075-.304.23-.304.401v7.358c0 .17.118.327.304.401a.636.636 0 0 0 .57-.05l5.67-3.68a.423.423 0 0 0 .206-.35.423.423 0 0 0-.206-.35z'
+      fill='currentColor'
+    />
+  </svg>
+)
+
+export default SvgPlayStroke
