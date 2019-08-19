8e8d086f9ee36feb41e3e101d1054140c549dd4a
Justin Jones <justinjones53@gmail.com>
2019-08-19 10:48:03 -0700

    Toast render (#541)
    Adds a toast component for displaying (and stacking) toast notifications. In the future, we'd like to implement a queueing system to allow a max number of notifications on screen at a time.

---
+++ src/components/Toast/index.js
@@ -0,0 +1,113 @@
+import React, { PureComponent } from 'react'
+import { createPortal } from 'react-dom'
+import cn from 'classnames'
+import PropTypes from 'prop-types'
+
+import { PROP_TYPE_CHILDREN } from '../constants'
+
+
+// TODO JJ: figure out if this violates best practices
+const existingToaster = document.querySelectorAll('mc-toaster')
+let toaster
+
+if (existingToaster.length) {
+  [toaster] = existingToaster
+} else {
+  toaster = document.createElement('div')
+  toaster.className = 'mc-toaster container mc-pb-8'
+  document.body.appendChild(toaster)
+}
+
+
+export default class Button extends PureComponent {
+  static propTypes = {
+    children: PROP_TYPE_CHILDREN,
+    className: PropTypes.string,
+    kind: PropTypes.oneOf([
+      'default',
+      'success',
+      'error',
+    ]),
+    show: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    kind: 'default',
+  }
+
+  toastRef = React.createRef()
+
+  state = {
+    show: false,
+  }
+
+  show = () => {
+    this.setState({ show: true })
+    this.timer = window.setTimeout(this.hide, 6000)
+  }
+
+  hide = () => {
+    this.setState({ show: false })
+  }
+
+  componentDidMount () {
+    if (this.props.show) {
+      this.show()
+    }
+
+    this.setState({
+      height: this.toastRef.current.offsetHeight,
+    })
+  }
+
+  componentWillReceiveProps (newProps) {
+    if (newProps.show && newProps.show !== this.props.show) {
+      this.show()
+    }
+  }
+
+  pauseHide = () => {
+    window.clearTimeout(this.timer)
+  }
+
+  resumeHide = () => {
+    if (this.state.show) {
+      this.show()
+    }
+  }
+
+  render () {
+    const {
+      children,
+      className,
+      kind,
+    } = this.props
+
+    const {
+      show,
+      height,
+    } = this.state
+
+    const classNames = cn({
+      'mc-toast mc-mt-2 mc-py-1 mc-px-3': true,
+      'mc-toast--show': show,
+      [`mc-toast--${kind}`]: kind,
+      [className]: className,
+    })
+
+    return createPortal(
+      <div
+        className={classNames}
+        onMouseOver={this.pauseHide}
+        onMouseOut={this.resumeHide}
+        ref={this.toastRef}
+        style={{ maxHeight: height }}
+      >
+        <div className='mc-toast__content'>
+          {children}
+        </div>
+      </div>,
+      toaster,
+    )
+  }
+}
---
+++ src/components/Toast/index.stories.js
@@ -0,0 +1,74 @@
+import React, { PureComponent } from 'react'
+import { storiesOf } from '@storybook/react'
+import { sample } from 'lodash'
+
+import withAddons from '../../utils/withAddons'
+import DocHeader from '../../utils/DocHeader'
+
+import Toast from '../Toast'
+import Button from '../Button'
+import Icon from '../Icons'
+
+
+const sampleKinds = ['default', 'default', 'default', 'default', 'success', 'error']
+const sampleIcons = ['check', 'info', undefined, undefined, undefined, undefined]
+const sampleTexts = [
+  'Hello!',
+  'And good day!',
+  'I SAID GOOD DAY!!!',
+  `Quis esse et laboris anim nulla ea nostrud sunt aute nulla
+  adipisicing enim culpa est qui amet fugiat duis minim officia
+  commodo ad culpa consectetur eiusmod aliqua et est esse esse.`,
+]
+
+
+class ToastStory extends PureComponent {
+  state = {
+    toasts: [],
+  }
+
+  handleClick = () => {
+    const newToast = {
+      kind: sample(sampleKinds),
+      icon: sample(sampleIcons),
+      text: sample(sampleTexts),
+    }
+
+    this.setState(prevState => ({
+      toasts: [...prevState.toasts, newToast],
+    }))
+  }
+
+  render () {
+    const { toasts } = this.state
+
+    return (
+      <div className='container'>
+        <DocHeader
+          title='Toast'
+          description='Part of a balanced breakfast.'
+        />
+
+        <Button onClick={this.handleClick}>
+          Show Toast
+        </Button>
+
+        {toasts.map((toast, key) =>
+          <Toast show kind={toast.kind} key={key}>
+            {toast.icon &&
+              <Icon kind={toast.icon} className='mc-mr-2' />
+            }
+            {toast.text}
+          </Toast>,
+        )}
+      </div>
+    )
+  }
+}
+
+
+storiesOf('Components|Toast', module)
+  .add('Summary', withAddons({
+    path: 'components/Toast/index.stories.js',
+    component: Toast,
+  })(() => <ToastStory />))
