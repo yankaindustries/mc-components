f2f962c4c13e7b4bfaf79e8aa2d5691d160ca630
Justin Jones <justinjones53@gmail.com>
2019-08-27 13:37:48 -0700

    adding function as valid child proptype (#543)

--- src/components/constants.js
+++ src/components/constants.js
@@ -2,7 +2,8 @@ import PropTypes from 'prop-types'
 
 
 export const PROP_TYPE_CHILDREN = PropTypes.oneOfType([
+  PropTypes.func,
   PropTypes.node,
-  PropTypes.arrayOf(PropTypes.node),
   PropTypes.arrayOf(PropTypes.func),
+  PropTypes.arrayOf(PropTypes.node),
 ])
