d6c02f816e016482adc8dea7bb0f990f10692be8
bcfancher-masterclass <brandon.fancher@masterclass.com>
2019-10-07 18:06:42 -0700

    fixed some linter stuffs

--- src/components/Loader/index.stories.js
+++ src/components/Loader/index.stories.js
@@ -7,7 +7,6 @@ import DocSection from '../../utils/DocSection'
 import Loader from '.'
 
 class LoaderStory extends PureComponent {
-
   render () {
     return (
       <div className='container'>
@@ -19,8 +18,8 @@ class LoaderStory extends PureComponent {
         <DocSection title='Demo'>
           <div className='row'>
             <div className='col-2'>
-              <Loader 
-                color={"#cc0000"}
+              <Loader
+                color={'#cc0000'}
               />
             </div>
           </div>
