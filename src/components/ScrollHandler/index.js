3c14d67145bd6e56814c5d8f51e6e438256bc5e6
bcfancher-mc <56046844+bcfancher-mc@users.noreply.github.com>
2019-10-16 14:27:45 -0700

    new ScrollHandler component (#593)
    * new ScrollHandler component

---
+++ src/components/ScrollHandler/index.js
@@ -0,0 +1,46 @@
+import { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+import { PROP_TYPE_CHILDREN } from '../constants'
+import { renderChildren } from '../helpers'
+
+
+export default class ScrollHandler extends PureComponent {
+  static propTypes = {
+    to: PropTypes.number,
+    children: PROP_TYPE_CHILDREN.isRequired,
+  }
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      scrolled: false,
+    }
+
+    window.addEventListener('scroll', this.checkIfSufficientlyScrolled)
+  }
+
+  componentDidMount () {
+    this.checkIfSufficientlyScrolled()
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.checkIfSufficientlyScrolled)
+  }
+
+  checkIfSufficientlyScrolled = () => {
+    const scrollY = window.pageYOffset || document.documentElement.scrollTop
+
+    this.setState({
+      scrolled: scrollY > this.props.to,
+    })
+  }
+
+  render () {
+    const { children } = this.props
+    const { scrolled } = this.state
+
+    return renderChildren(children, { scrolled })
+  }
+}
