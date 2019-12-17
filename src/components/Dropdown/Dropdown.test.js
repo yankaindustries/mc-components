41215ebcca9ad80d99ceda1a92792bacac69ad29
bcfancher-mc <brandon.fancher@masterclass.com>
2019-12-17 10:06:11 -0800

    added some tests for content body/header/footer

--- src/components/Dropdown/Dropdown.test.js
+++ src/components/Dropdown/Dropdown.test.js
@@ -4,12 +4,23 @@ import propTypes from 'prop-types'
 import {
   Dropdown,
   DropdownBody,
+  DropdownHeader,
+  DropdownFooter,
+  DropdownItem,
   DropdownToggle,
   DropdownContent,
   DropdownContentControlled,
 } from '../index'
 
 describe('Dropdown', () => {
+  const CLASS_DROPDOWN_ACTIVE = 'mc-dropdown--active'
+  const CLASS_DROPDOWN_HTML_OPEN = 'mc-dropdown__html--open'
+
+  const dropdownIsVisible = (rootHtml, dropdownElem) => (
+    rootHtml.classList.contains(CLASS_DROPDOWN_HTML_OPEN) &&
+    dropdownElem.classList.contains(CLASS_DROPDOWN_ACTIVE)
+  )
+
   describe('DropdownContent', () => {
     const setup = () => {
       const toggleText = 'Click To Toggle'
@@ -33,21 +44,19 @@ describe('Dropdown', () => {
       }
     }
 
-    it('renders default dropdown with popper', () => {
+    it('renders default dropdown and toggles', () => {
       // given a dropdown
       const { toggleElem, dropdownElem } = setup()
       const rootHtml = document.getElementsByTagName('html')[0]
-      expect(dropdownElem).not.toHaveClass('mc-dropdown--active')
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeFalsy()
 
       // after the toggle is clicked
       fireEvent.click(toggleElem)
-      expect(dropdownElem).toHaveClass('mc-dropdown--active')
-      expect(rootHtml).toHaveClass('mc-dropdown__html--open')
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeTruthy()
 
       // after clicking outside the dropdown
       fireEvent.click(document.body)
-      expect(dropdownElem).not.toHaveClass('mc-dropdown--active')
-      expect(rootHtml).not.toHaveClass('mc-dropdown__html--open')
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeFalsy()
     })
   })
 
@@ -107,21 +116,99 @@ describe('Dropdown', () => {
       }
     }
 
-    it('renders default, controlled dropdown with popper', () => {
+    it('renders default, controlled dropdown and toggles', () => {
       // given a dropdown
       const { toggleElem, dropdownElem } = setup()
       const rootHtml = document.getElementsByTagName('html')[0]
-      expect(dropdownElem).not.toHaveClass('mc-dropdown--active')
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeFalsy()
 
       // after the toggle is clicked
       fireEvent.click(toggleElem)
-      expect(dropdownElem).toHaveClass('mc-dropdown--active')
-      expect(rootHtml).toHaveClass('mc-dropdown__html--open')
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeTruthy()
 
       // after clicking outside the dropdown
       fireEvent.click(document.body)
-      expect(dropdownElem).not.toHaveClass('mc-dropdown--active')
-      expect(rootHtml).not.toHaveClass('mc-dropdown__html--open')
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeFalsy()
+    })
+  })
+
+  describe('DropdownBody & stuffs', () => {
+    const toggleText = 'Click To Toggle'
+    const contentHeader = 'Some Header'
+    const contentBody = 'Some Content'
+    const contentFooter = 'Some Footer'
+    let dropdownItemClicked
+
+    beforeEach(() => {
+      dropdownItemClicked = false
+    })
+
+    const clickItem = () => {
+      console.log('DROPDOWN ITEM CLICKED!!!')
+      dropdownItemClicked = true
+    }
+
+    const setup = () => {
+      const utils = render(
+        <Dropdown>
+          <DropdownToggle><span>{toggleText}</span></DropdownToggle>
+          <DropdownContent>
+            <DropdownHeader><span>{contentHeader}</span></DropdownHeader>
+            <DropdownBody>
+              <DropdownItem
+                onClick={clickItem}
+                closeOnClick={true}
+              >
+                <span>{contentBody}</span>
+              </DropdownItem>
+              </DropdownBody>
+            <DropdownFooter><span>{contentFooter}</span></DropdownFooter>
+          </DropdownContent>
+        </Dropdown>,
+      )
+      const toggleElem = utils.getByText(toggleText)
+      const dropdownElem = utils.getByRole('dropdown')
+
+      return {
+        toggleElem,
+        dropdownElem,
+        ...utils,
+      }
+    }
+
+    it('renders default dropdown and toggles', () => {
+      // given a dropdown
+      const { toggleElem, dropdownElem } = setup()
+      const rootHtml = document.getElementsByTagName('html')[0]
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeFalsy()
+
+      // after the toggle is clicked
+      fireEvent.click(toggleElem)
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeTruthy()
+
+      // after clicking outside the dropdown
+      fireEvent.click(document.body)
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeFalsy()
+    })
+
+    it('renders Header, Body & Footer and closes when DropdownItem is clicked', () => {
+      // given a dropdown
+      const { getByText, toggleElem, dropdownElem } = setup()
+      const rootHtml = document.getElementsByTagName('html')[0]
+
+      // ensure everything is getting rendered
+      expect(getByText(contentHeader)).not.toBeNull()
+      expect(getByText(contentBody)).not.toBeNull()
+      expect(getByText(contentFooter)).not.toBeNull()
+
+      // after the toggle is clicked
+      fireEvent.click(toggleElem)
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeTruthy()
+
+      // clicking on the DropdownItem should close
+      fireEvent.click(getByText(contentBody))
+      expect(dropdownItemClicked).toBeTruthy()
+      expect(dropdownIsVisible(rootHtml, dropdownElem)).toBeFalsy()
     })
   })
 })
