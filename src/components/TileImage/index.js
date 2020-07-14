a2475eec66becc0398235a258ac9c1dacf54e3b7
Andy Axton <dizzysfarm@gmail.com>
2020-07-14 12:36:32 -0700

    remove children from prop types

--- src/components/TileImage/index.js
+++ src/components/TileImage/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
-import { PROP_TYPE_CHILDREN } from '../constants'
 
 const TileImage = ({ className, image, imageUrl }) => {
   const classes = cn('mc-tile-image', className)
@@ -12,7 +11,6 @@ const TileImage = ({ className, image, imageUrl }) => {
 
 TileImage.propTypes = {
   className: PropTypes.string,
-  children: PROP_TYPE_CHILDREN,
   image: PropTypes.node,
   imageUrl: PropTypes.string,
 }
