16ed78ec4924618c345b2b5d440ceaf06dcc1388
Andy Axton <dizzysfarm@gmail.com>
2020-07-14 12:26:50 -0700

    convert to functional component

--- src/components/TileImage/index.js
+++ src/components/TileImage/index.js
@@ -1,33 +1,24 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
-
 import { PROP_TYPE_CHILDREN } from '../constants'
 
-
-export default class TileImage extends PureComponent {
-  static propTypes = {
-    className: PropTypes.string,
-    children: PROP_TYPE_CHILDREN,
-    image: PropTypes.node,
-    imageUrl: PropTypes.string,
-  }
-
-  static defaultProps = {
-    className: '',
-  }
-
-  render () {
-    const {
-      className,
-      image,
-      imageUrl,
-    } = this.props
-
-    const classes = cn('mc-tile-image', className)
-
-    return image
-      ? React.cloneElement(image, { className: classes })
-      : <img className={classes} src={imageUrl} />
-  }
+const TileImage = ({ className, image, imageUrl }) => {
+  const classes = cn('mc-tile-image', className)
+  return image
+    ? React.cloneElement(image, { className: classes })
+    : <img className={classes} src={imageUrl} />
 }
+
+TileImage.propTypes = {
+  className: PropTypes.string,
+  children: PROP_TYPE_CHILDREN,
+  image: PropTypes.node,
+  imageUrl: PropTypes.string,
+}
+
+TileImage.defaultProps = {
+  className: '',
+}
+
+export default TileImage
