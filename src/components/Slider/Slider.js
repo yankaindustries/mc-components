6d3e3e6af102cc745121fdb0b8db0e20fb4f3f1b
Justin Jones <justinjones53@gmail.com>
2020-07-27 16:55:27 -0700

    Video (#746)
    * POC for new video player
    
    * moar updates to the video component
    
    * Abstracting Video controls and settings to separate components
    
    * updating UI to better fit existing VP
    
    * better slider and added volume
    
    * moved to proper directories, abstracted slider, fun volume UI
    
    * video ux improvements
    
    * this belongs in the masterclass repo
    
    * this is not needed either...

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const Slider = ({
+  value,
+  buffer,
+  onChange,
+}) => {
+  const handleScrubberInteraction = scrubber => (event) => {
+    const {
+      left: scrubberLeft,
+      width: scrubberWidth,
+    } = scrubber.getBoundingClientRect()
+
+    const {
+      clientX: eventLeft,
+    } = event
+
+    const left = Math.min(Math.max(0, eventLeft - scrubberLeft), scrubberWidth)
+    const value = left / scrubberWidth
+
+    onChange(value)
+  }
+
+  const handleScrubberMouseDown = (event) => {
+    const listener = handleScrubberInteraction(event.currentTarget)
+
+    handleScrubberInteraction(event.currentTarget)(event)
+
+    window.addEventListener('mousemove', listener)
+    window.addEventListener('mouseup', () => {
+      window.removeEventListener('mousemove', listener)
+    })
+  }
+
+  const bufferPerc = `${buffer * 100}%`
+  const valuePerc = `${value * 100}%`
+
+  return (
+    <div
+      className='mc-slider'
+      onMouseDown={handleScrubberMouseDown}
+    >
+      <div className='mc-slider__bar'>
+        <div className='mc-slider__buffer' style={{ width: bufferPerc }} />
+        <div className='mc-slider__fill' style={{ width: valuePerc }} />
+        <div className='mc-slider__handle' style={{ left: valuePerc }} />
+      </div>
+    </div>
+  )
+}
+
+Slider.propTypes = {
+  value: PropTypes.number.isRequired,
+  buffer: PropTypes.number,
+  onChange: PropTypes.func.isRequired,
+}
+
+export default Slider
---
+++ src/components/Video/Video.js
@@ -0,0 +1,70 @@
+import React, { useRef } from 'react'
+import PropTypes from 'prop-types'
+import cn from 'classnames'
+
+import useVideo from './useVideo'
+import VideoControls from './VideoControls'
+import VideoScreens from './VideoScreens'
+
+
+export const STATE_IDLE = 'idle'
+export const STATE_PLAYING = 'playing'
+export const STATE_PAUSED = 'paused'
+export const STATE_ENDED = 'ended'
+export const STATE_ERROR = 'error'
+
+const FIT_FIT = 'fit'
+const FIT_COVER = 'cover'
+const FIT_FILL = 'fill'
+
+export const VideoContext = React.createContext()
+
+
+const Video = ({
+  videoRef: passedVideoRef,
+  fit = FIT_FIT,
+  ...props
+}) => {
+  const videoRef = useRef(passedVideoRef)
+  const containerRef = useRef(null)
+
+  const video = useVideo(videoRef, containerRef)
+
+  const classes = cn({
+    'mc-video': true,
+    [`mc-video--${fit}`]: fit,
+  })
+
+  return (
+    <VideoContext.Provider value={video}>
+        <div
+          ref={containerRef}
+          className={classes}
+          onClick={video.togglePlay}
+          onDoubleClick={video.toggleFullscreen}
+        >
+          <video
+            ref={videoRef}
+            className='mc-video__video'
+            crossOrigin='anonymous'
+            preload='metadata'
+            {...props}
+            controls={false}
+          />
+
+          <VideoControls {...props} />
+          <VideoScreens {...props} />
+        </div>
+    </VideoContext.Provider>
+  )
+}
+
+
+Video.propTypes = {
+  children: PropTypes.any,
+  fit: PropTypes.oneOf([FIT_FIT, FIT_COVER, FIT_FILL]),
+  videoRef: PropTypes.func,
+}
+
+
+export default Video
