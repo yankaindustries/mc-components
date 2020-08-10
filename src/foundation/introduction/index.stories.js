a6960ad8a24ae75ff0bd1778a944d161a305e726
Stephen Pontes <stephen@shibbay.com>
2020-08-10 15:20:01 -0700

    Rebrand (#749)

--- src/foundation/introduction/index.stories.js
+++ src/foundation/introduction/index.stories.js
@@ -12,7 +12,7 @@ storiesOf('Introduction|Introduction', module)
   .add('Getting Started', () =>
     <div className='container'>
       <DocHeader
-        title='mc-components'
+        title='mc-ui'
         description='UI library for for media heavy applications.'
       />
 
@@ -24,14 +24,14 @@ storiesOf('Introduction|Introduction', module)
           {' '}
           <a
             className='mc-text--link'
-            href='//npmjs.org/package/mc-components'
+            href='//npmjs.org/package/mc-ui'
           >
             NPM package
           </a>
         </p>
 
         <Highlight className='language-bash mc-mb-8'>
-          yarn add mc-components
+          yarn add mc-ui
         </Highlight>
       </DocSection>
 
@@ -41,17 +41,17 @@ storiesOf('Introduction|Introduction', module)
         </p>
 
         <Highlight className='language-scss mc-mb-8'>
-          {'@import \'mc-components/src/styles/index\''}
+          {'@import \'mc-ui/styles\''}
         </Highlight>
 
         <p className='mc-mb-2'>
           Sometimes you&apos;ll need to reference color variables
-          or breakpoints. You can import the &ldquo;base&rdquo; scss
+          or breakpoints. You can import the &ldquo;base&rdquo; SCSS
           file (which does not add any weight to your page) like so:
         </p>
 
         <Highlight className='language-scss mc-mb-8'>
-          {'@import \'mc-components/src/styles/base/base\';'}
+          {'@import \'mc-ui/styles/base\''}
         </Highlight>
 
         <p className='mc-mb-2'>
@@ -59,14 +59,14 @@ storiesOf('Introduction|Introduction', module)
         </p>
 
         <Highlight className='language-jsx mc-mb-8'>
-  {`import React from 'react'
-  import { Button } from 'mc-components'
+{`import React from 'react'
+import { Button } from 'mc-ui'
 
-  const App = () => (
-    <Button>
-      Hello World
-    </Button>
-  )`}
+const App = () => (
+  <Button>
+    Hello World
+  </Button>
+)`}
         </Highlight>
       </DocSection>
     </div>,
