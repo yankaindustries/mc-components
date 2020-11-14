ca38d171e289c01b12593e6e11fc05967e0d3a24
justinjones53@gmail.com <justinjones53@gmail.com>
2020-11-13 16:15:08 -0800

    moar badge styles

--- src/components/Tags/index.js
+++ src/components/Tags/index.js
@@ -86,7 +86,7 @@ const Tags = ({
     >
       {collection.map((item, index) => (
         <div className='mc-form-tags__tag' key={item}>
-          <Badge kind='bright'>
+          <Badge kind='default'>
             {item}
             <Icon
               kind='close'
